3cf3b82db386b7d730172539f6253ce7661b5872
nostraJello <angeloufuentes751@gmail.com>
2024-10-01 22:28:01 +0800

    <0921>_<#13>_<EagerLoadingAndTheN+1Problem>

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -3,13 +3,16 @@
         Job Listings
     </x-slot:heading>
 
-    <ul>
+    <div class="space-y-4">
         @foreach ($jobs as $job)
-            <li>
-                <a href="/jobs/{{ $job['id'] }}">
-                    <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }}
+                <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+                    <div class="font-bold text-blue-500 text-sm">
+                        {{ $job->employer->name }}
+                    </div>
+                    <div>
+                        <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }}
+                    </div>
                 </a>
-            </li>
         @endforeach
-    </ul>
+    </div>
 </x-layout>
