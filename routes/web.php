50e02ce02d8db83f69062d0700cbd9105f2417de
nostraJello <angeloufuentes751@gmail.com>
2024-10-04 20:23:52 +0800

    <1005>_<#25>_<QueuesAreEasierThenYouThink>

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,17 @@
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
+use App\Jobs\TranslateJob;
+use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 
 Route::get('test', function () {
-    return new \App\Mail\JobPosted();
-});
+    $job = Job::first();
 
+    TranslateJob::dispatch($job);
+
+    return 'Done';
+});
 
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
@@ -32,3 +37,4 @@ Route::post('/register', [RegisteredUserController::class, 'store']);
 Route::get('/login', [SessionController::class, 'create'])->name('login');
 Route::post('/login', [SessionController::class, 'store']);
 Route::post('/logout', [SessionController::class, 'destroy']);
+
