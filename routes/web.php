0aa89a74395db9e3e8cb453b0730a8cb449a28f9
nostraJello <angeloufuentes751@gmail.com>
2024-10-04 20:17:20 +0800

    <1005>_<#24>_<HowToPreviewAndSendEmailUsingMailableClasses>

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,11 @@ use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('test', function () {
+    return new \App\Mail\JobPosted();
+});
+
+
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
