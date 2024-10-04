fc620b866a07c8e457b8d2461ff221579649e983
nostraJello <angeloufuentes751@gmail.com>
2024-10-05 02:53:00 +0800

    0830_#5_ConditionalsAndBooleans

--- index.php
+++ index.php
@@ -3,16 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <title>Demo</title>
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
 </head>
 <body>
+    <?php
+        $name = "Dark Matter";
+        $read = true;
+
+        if ($read) {
+           $message = "You have read $name";
+        } else {
+            $message = "You have NOT read $name";
+        }
+    ?>
 
     <h1>
-        <?php
-            $greeting = "Hello";
-            $noun = "World";
-
-            echo "$greeting $noun" . "!"
-        ?> 
+        <?= $message ?>
     </h1>
 
 </body>
