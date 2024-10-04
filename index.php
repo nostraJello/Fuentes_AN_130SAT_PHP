6f18fdbc7be3694a74990646a407ce1fd55fc23a
nostraJello <angeloufuentes751@gmail.com>
2024-10-05 02:57:47 +0800

    0830_#7_AssociativeArrays

--- index.php
+++ index.php
@@ -4,22 +4,33 @@
     <meta charset="UTF-8">
     <title>Demo</title>
 </head>
-<body>
-    <h1>Recommended Books</h1>
 
+<body>
     <?php
         $books = [
-            "Do Androids Dream of Electric Sheep",
-            "The Langoliers",
-            "Hail Mary"
+            [
+                'name' => 'Do Androids Dream of Electric Sheep',
+                'author' => 'Philip K. Dick',
+                'releaseYear' => 1968,
+                'purchaseUrl' => 'http://example.com'
+            ],
+            [
+                'name' => 'Project Hail Mary',
+                'author' => 'Andy Weir',
+                'releaseYear' => 2021,
+                'purchaseUrl' => 'http://example.com'
+            ]
         ];
     ?>
 
     <ul>
         <?php foreach ($books as $book) : ?>
-            <li><?= $book ?></li>
+            <li>
+                <a href="<?= $book['purchaseUrl'] ?>">
+                    <?= $book['name'] ?> (<?= $book['releaseYear'] ?>)
+                </a>
+            </li>
         <?php endforeach; ?>
     </ul>
-
 </body>
 </html>
