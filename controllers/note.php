d215c1bd597e9fc9d09f7977dd7fd8371bc26930
nostraJello <angeloufuentes751@gmail.com>
2024-10-05 16:47:40 +0800

    0928_#24_ProgrammingIsRewriting

--- controllers/note.php
+++ controllers/note.php
@@ -8,12 +8,8 @@ $currentUserId = 1;
 
 $note = $db->query('select * from notes where id = :id', [
     'id' => $_GET['id']
-])->fetch();
-if (! $note) {
-    abort();
-}
-if ($note['user_id'] !== $currentUserId) {
-    abort(Response::FORBIDDEN);
-}
+])->findOrFail();
+
+authorize($note['user_id'] === $currentUserId);
 
 require "views/note.view.php";
