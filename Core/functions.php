3a20efa4aff09273e1bd2e540f3e04642be337a2
nostraJello <angeloufuentes751@gmail.com>
2024-10-05 17:16:49 +0800

    1005_#31_NamespacingWhatWhyHow?

--- Core/functions.php
+++ Core/functions.php
@@ -21,14 +21,14 @@ function authorize($condition, $status = Response::FORBIDDEN)
     }
 }
 
-}
 function base_path($path)
 {
     return BASE_PATH . $path;
 }
+
 function view($path, $attributes = [])
 {
     extract($attributes);
-    
+
     require base_path('views/' . $path);
 }
--- routes.php
+++ routes.php
@@ -1,10 +1,34 @@
 <?php
 
-return [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/notes' => 'controllers/notes/index.php',
-    '/note' => 'controllers/notes/show.php',
-    '/notes/create' => 'controllers/notes/create.php',
-    '/contact' => 'controllers/contact.php',
-];
+function dd($value)
+{
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+
+    die();
+}
+
+function urlIs($value)
+{
+    return $_SERVER['REQUEST_URI'] === $value;
+}
+
+function authorize($condition, $status = Response::FORBIDDEN)
+{
+    if (! $condition) {
+        abort($status);
+    }
+}
+
+function base_path($path)
+{
+    return BASE_PATH . $path;
+}
+
+function view($path, $attributes = [])
+{
+    extract($attributes);
+
+    require base_path('views/' . $path);
+}
